fb69b698f024fba1f9d56a0a25cc7fb2f3c0c13c
Elies <elies.hariate@epitech.eu>
2023-10-26 14:25:51 +0200

    feat(API) : Adding role and team to USER, Fixing Clock controller

--- api/lib/api_web/controllers/clock_controller.ex
+++ api/lib/api_web/controllers/clock_controller.ex
@@ -7,12 +7,28 @@ defmodule ApiWeb.ClockController do
   action_fallback ApiWeb.FallbackController
 
   def show(conn, %{"userID" => user_id}) when is_binary(user_id) do
-    clocks = Accounts.list_user_clocks(user_id)
-    render(conn, "index.json", clocks: clocks)
+    user_id = Ecto.UUID.cast(user_id)
+
+    case user_id do
+      {:ok, uuid} ->
+        clocks = Accounts.list_user_clocks(uuid)
+        render(conn, "index.json", clocks: clocks)
+
+      :error ->
+        conn
+        |> put_status(400)
+        |> json(%{error: "Invalid UUID format for user ID"})
+    end
   end
 
+
   def create(conn, %{"userID" => user_id, "clock" => clock_params}) when is_binary(user_id) do
-    case Accounts.create_user_clock(user_id, clock_params) do
+    last_clock = Accounts.get_last_user_clock(user_id)
+
+    status = if last_clock && last_clock.status == false, do: true, else: false
+    new_clock_params = Map.put(clock_params, "status", status)
+
+    case Accounts.create_user_clock(user_id, new_clock_params) do
       {:ok, %Clock{} = clock} ->
         conn
         |> put_status(:created)
@@ -25,4 +41,6 @@ defmodule ApiWeb.ClockController do
         |> render(ApiWeb.ChangesetView, "error.json", changeset: changeset)
     end
   end
+
+
 end
--- api/priv/repo/migrations/20231023090030_create_users.exs
+++ api/priv/repo/migrations/20231023090030_create_users.exs
@@ -6,9 +6,12 @@ defmodule Api.Repo.Migrations.CreateUsers do
       add :id, :binary_id, primary_key: true
       add :username, :string
       add :email, :string, null: false
+      add :role, :string, default: "user"
+      add :team, :binary_id, default: nil
 
       timestamps()
     end
+
     create unique_index(:users, [:email])
     execute("ALTER TABLE users ADD CONSTRAINT email_formatcheck CHECK (email ~* '^[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$')")
   end
