a6929c360a2a9c2d556135fe8ec6a868e7be2ca0
Musubi42 <thibaut.raphael@protonmail.com>
2023-10-26 12:27:04 +0200

    feat(CSS) : Add tailwind custom color and font

--- timemanager-front/tailwind.config.js
+++ timemanager-front/tailwind.config.js
@@ -2,8 +2,17 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#EE74DA',
+        secondary: '#403F4C',
+        ternary: '#51E5FF'
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'Arial', 'sans-serif'],
+        'firas-sans': ['Fira Sans', 'serif']
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
-
