f06aaa0f266421374f5add6f75fa57839fbd1066
Elies <elies.hariate@epitech.eu>
2023-10-30 15:06:45 +0100

    feat(front) : integration workingtimes/workingtime

---
+++ tm-front/src/components/Workingtime.vue
@@ -0,0 +1,167 @@
+<template>
+  <div>
+    <div v-if="workData">
+      <div class="work-entry flex flex-row gap-5">
+        <div class="start">
+          Start: {{ workData.start }}
+        </div>
+        <div class="end">
+          End: {{ workData.end }}
+        </div>
+        <button @click="deleteWorkingTime(workData.id)" class="suppr">
+          Supprimer
+        </button>
+        <button v-if="workData.id" @click="openModifyModal(workData)" class="suppr">
+          Modifier
+        </button>
+      </div>
+    </div>
+    <p v-else-if="loading">Chargement des données...</p>
+    <p v-else>
+      <button @click="openCreateModal" class="suppr">
+        Créer Working Time
+      </button>
+    </p>
+
+    <ModifyModal v-if="ModifyModalOpen" :title="'Modify WorkingTime'" @close="closeModifyModalOpen" @modify="updateWorkingTime">
+      <div>
+        <input v-model="formattedStartDate" type="text" placeholder="Start Date" class="mb-2 p-2 rounded border border-gray-300 mt-2">
+        <input v-model="formattedEndDate" type="text" placeholder="End Date" class="p-2 rounded border border-gray-300">
+      </div>
+    </ModifyModal>
+
+    <CreateModal v-if="CreateModalOpen" :title="'Create WorkingTime'" @close="closeCreateModal" @create="createWorkingTime">
+      <div class="flex flex-col">
+        <input v-model="createStartDate" type="text" placeholder="Start Date" class="mb-2 p-2 rounded border border-gray-300 mt-2">
+        <input v-model="createEndDate" type="text" placeholder="End Date" class="p-2 rounded border border-gray-300">
+      </div>
+    </CreateModal>
+  </div>
+</template>
+
+<script setup>
+import ModifyModal from '../components/ModifyWorkingTime.vue';
+import CreateModal from '../components/CreateWorkingTime.vue';
+import { ref, onBeforeMount, reactive } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import axios from 'axios';
+import { format } from 'date-fns';
+
+const route = useRoute();
+const router = useRouter();
+const userId = route.params.userid;
+const workingTimeId = route.params.workingtimeid;
+let workData = ref(null);
+let loading = ref(false);
+let ModifyModalOpen = ref(false);
+let CreateModalOpen = ref(false);
+let createStartDate = ref("");
+let createEndDate = ref("");
+let selectedWork = reactive({});
+let formattedStartDate = '';
+let formattedEndDate = '';
+
+function openModifyModal(work) {
+  ModifyModalOpen.value = true;
+  formattedStartDate = work.start;
+  formattedEndDate = work.end;
+  selectedWork = { ...work };
+}
+
+function openCreateModal() {
+  CreateModalOpen.value = true;
+}
+
+function closeCreateModal() {
+  CreateModalOpen.value = false;
+}
+
+function closeModifyModalOpen() {
+  ModifyModalOpen.value = false;
+}
+
+const formatDateTime = (datetime) => {
+  return format(new Date(datetime), 'yyyy-MM-dd HH:mm:ss');
+};
+
+async function getWorkingTime(userId, wtId) {
+  try {
+    loading.value = true;
+    const response = await axios.get(`http://localhost:4000/api/workingtimes/${userId}/${wtId}`);
+    if (response.status === 200) {
+      workData.value = response.data.data;
+      console.log(workData);
+    } else {
+      console.error('La requête GET a échoué', response);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la requête GET', error);
+  } finally {
+    loading.value = false;
+  }
+}
+
+async function deleteWorkingTime(id) {
+  try {
+    const response = await axios.delete(`http://localhost:4000/api/workingtimes/${id}`);
+    if (response.status === 204) {
+      console.log('La suppression a réussi', response);
+      await getWorkingTime(userId, workingTimeId);
+      router.push(`/workingtimes/${userId}`);
+    } else {
+      console.error('La suppression a échoué', response);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la suppression', error);
+  }
+}
+
+const updateWorkingTime = async () => {
+  try {
+    const { id } = selectedWork;
+
+    const body = {
+      working_time: {
+        start: formattedStartDate,
+        end: formattedEndDate,
+      },
+    };
+
+    const response = await axios.put(`http://localhost:4000/api/workingtimes/${id}`, body);
+
+    if (response.status === 200) {
+      console.log('La modification a réussi', response);
+      await getWorkingTime(userId, workingTimeId);
+    } else {
+      console.error('La modification a échoué', response);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la modification', error);
+  }
+};
+
+const createWorkingTime = async () => {
+  try {
+    const newWorkingTimeData = {
+    working_time: {
+      start: createStartDate.value,
+      end: createEndDate.value
+    }};
+    const response = await axios.post(`http://localhost:4000/api/workingtimes/${userId}`, newWorkingTimeData);
+    if (response.status === 201) {
+      console.log('La création a réussi', response);
+      router.push(`/workingtimes/${userId}`);
+    } else {
+      console.error('La création a échoué', response);
+    }
+  } catch (error) {
+    console.error('Erreur lors de la création', error);
+  }
+};
+
+onBeforeMount(async () => {
+  if (userId && workingTimeId) {
+    await getWorkingTime(userId, workingTimeId);
+  }
+});
+</script>
---
+++ tm-front/src/pages/Workingtime.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <WorkingTime/>
+    </div>
+  </template>
+  
+  <script setup >
+  import { ref, onBeforeMount } from 'vue';
+  import { useRoute, useRouter } from 'vue-router'
+  import WorkingTime from '@/components/Workingtime.vue';
+  
+  const router = useRouter();
+  
+  onBeforeMount(() => {
+    if (!localStorage.getItem('id')) {
+      router.push('/login');
+    }
+  });
+  </script>
+  L
