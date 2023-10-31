cbc48f5dff04f078ca5a46872ceb49d538b38aa7
Musubi42 <thibaut.raphael@protonmail.com>
2023-10-31 16:59:31 +0100

    Fix sript to launch back & front

--- run.sh
+++ run.sh
@@ -3,14 +3,10 @@
 # Depending on the param run either docker dev or docker prod
 if [ "$1" = "dev" ]; then
   # Run front
-  docker-compose -f ./tm-front/docker-compose.dev.yml up --build
-  # Run Back
-  docker-compose -f ./api/docker-compose.dev.yml up --build
+  docker-compose -f ./tm-front/docker-compose.dev.yml up --build -d && docker-compose -f ./api/docker-compose.dev.yml up --build -d
 elif [ "$1" = "prod" ]; then
-  # Run front
-  docker-compose -f ./tm-front/docker-compose.prod.yml up --build
-  # Run back
-  docker-compose -f ./api/docker-compose.prod.yml up --build
+  # Run front & back
+  docker-compose -f ./tm-front/docker-compose.prod.yml up --build -d && docker-compose -f ./api/docker-compose.prod.yml up --build -d
 else
   echo "Please provide a valid param"
 fi
