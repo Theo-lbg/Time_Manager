3034e1af6f8c0c5595bf27ccde099023585b6856
Théo Lebègue <84734200+Theo-lbg@users.noreply.github.com>
2023-10-31 16:19:03 +0100

    feat(add): add style for the navbar (#19)
    * feat(add): add style for the navbar
    
    * feat(title): add title and icon at the app

--- tm-front/src/components/Header.vue
+++ tm-front/src/components/Header.vue
@@ -1,25 +1,39 @@
 <template>
-    <nav class="px-2 py-3 bg-pink-500 mb-3">
-        <div class=" flex flex-wrap items-center justify-between">
-            <div class="w-full relative flex flex-row justify-between">
-                <li class="mx-2 flex items-center">
-                    <RouterLink class="ml-5" to="/">Dashboard</RouterLink>
-                </li>
-                <li class="mx-2 flex items-center">
-                    <RouterLink class="ml-5" to="/settings">Settings</RouterLink>
-                </li>
-                <li class="mx-2 flex items-center">
-                    <RouterLink :to="`/workingtimes/${userid}`">Feuille de temps</RouterLink>
-                </li>
-                <li class="mx-2 flex items-center">
-                    <RouterLink :to="`/clock/${userid}`">ClockManager</RouterLink>
-                </li>
-                <li class="mx-2 flex items-center">
-                    <RouterLink :to="`/workingtime/${userid}`">Créer un workingtime</RouterLink>
-                </li>
-                <li class="mx-2 flex items-center">
-                    <button @click="logoutUser">Déconnexion</button>
-                </li>
+    <nav class="flex justify-between p-2 px-4 bg-ternary">
+        <div class="flex justify-between items-center w-full ">
+            <div class="xl:w-auto">
+                <a class="block max-w-max" href="#">
+                    <img class="h-11" src="../assets/favicon.svg" alt="Pooger-logo">
+                </a>
+            </div>
+            <div class="hidden xl:block xl:w-auto mx-auto">
+                <ul class="flex justify-center">
+                    <li class="mr-12"><a class="ml-5 font-montserrat text-black hover:text-white">
+                            <RouterLink to="/">Dashboard</RouterLink>
+                        </a>
+                    </li>
+                    <li class="mr-12"><a class="ml-5 font-montserrat text-black hover:text-white" to="/settings" href="#">
+                            <RouterLink to="/settings">Settings</RouterLink>
+                        </a>
+                    </li>
+                    <li class="mr-12"><a class="ml-5 font-montserrat text-black hover:text-white"
+                            to="/workingtime/${userId}" href="#">
+                            <RouterLink :to="`/workingtimes/${userid}`">Feuille de temps
+                            </RouterLink>
+                        </a>
+                    </li>
+                    <li class="mr-12 flex items-center ml-5 font-montserrat text-black hover:text-white">
+                        <RouterLink :to="`/clock/${userid}`">ClockManager</RouterLink>
+                    </li>
+                    <li class="mr-12 flex items-center ml-5 font-montserrat text-black hover:text-white     ">
+                        <RouterLink :to="`/workingtime/${userid}`">Créer un temps de travail</RouterLink>
+                    </li>
+                </ul>
+            </div>
+            <div class="hidden xl:block xl:w-auto">
+                <div class="flex items-center justify-end"><a @click="logoutUser"
+                        class=" font-montserrat inline-block py-2 px-4 text-sm leading-5 text-green-50 bg-green-500 hover:bg-red-500 font-medium focus:ring-2 focus:ring-green-500 focus:ring-opacity-50 rounded-md"
+                        href="#">Déconnexion</a></div>
             </div>
         </div>
     </nav>
@@ -32,7 +46,7 @@ import { useRoute, useRouter } from 'vue-router'
 const router = useRouter()
 let userid = localStorage.getItem("id")
 const logoutUser = () => {
-  logout();
-  router.push("/login")
+    logout();
+    router.push("/login")
 };
 </script>
